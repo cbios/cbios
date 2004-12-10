2d72731c89b1decb8dab599a24727fac674fd027
Maarten ter Huurne <maarten@treewalker.org>
2004-12-10 01:13:56 +0000

    Initial version. Contains routine to print string to openMSX debug device.

---
+++ src/debug.asm
@@ -0,0 +1,47 @@
+; $Id$
+; C-BIOS debug routines
+; These routines should not be used in release builds of C-BIOS, but they can
+; be useful for developers and testers.
+;
+; Copyright (c) 2004 Maarten ter Huurne.  All rights reserved.
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions
+; are met:
+; 1. Redistributions of source code must retain the above copyright
+;    notice, this list of conditions and the following disclaimer.
+; 2. Redistributions in binary form must reproduce the above copyright
+;    notice, this list of conditions and the following disclaimer in the
+;    documentation and/or other materials provided with the distribution.
+;
+; THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+; IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+; IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+; NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+; THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;
+
+;--------------------------------
+; Print string to openMSX debug device.
+; Input:   HL = address of zero-terminated string
+; Changes: HL, AF
+print_debug:
+                ld      a,$23
+                out     ($2E),a
+print_debug_lp:
+                ld      a,(hl)
+                inc     hl
+                or      a
+                jr      z,print_debug_done
+                out     ($2F),a
+                jr      print_debug_lp
+print_debug_done:
+                ld      a,$00
+                out     ($2E),a
+                ret
+
