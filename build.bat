65e0460ef5d42e712f58eb332ef9f8c713cadd7f
Albert Beevendorp <bifimsx@users.sourceforge.net>
2004-12-12 21:05:21 +0000

    Symbol files are written to derived/lst now.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 tniasm %1.asm ..\derived\bin\cbios_%1.rom
 if errorlevel 1 goto error
-move tniasm.sym ..\derived\sym\cbios_%1.sym
+move tniasm.sym ..\derived\lst\cbios_%1.sym
 goto no_error
 :error
 pause
--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir derived\bin 2> NUL
-mkdir derived\sym 2> NUL
+mkdir derived\lst 2> NUL
 cd src
 FOR %%i IN (sub;main) DO call ..\build %%i
 del *.tmp >> NUL
