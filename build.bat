11665efcf09a51ffb50f0a69b193eb338e067864
Maarten ter Huurne <maarten@treewalker.org>
2004-12-18 17:01:40 +0000

    Added support for Pasmo to make.bat/build.bat.

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
+IF %2.==pasmo. GOTO PASM
 tniasm %1.asm ..\derived\bin\cbios_%1.rom
 if errorlevel 1 goto error
 move tniasm.sym ..\derived\lst\cbios_%1.sym
+goto no_error
+:PASM
+pasmo %1.asm ..\derived\bin\cbios_%1.rom
+if errorlevel 1 goto error
 goto no_error
 :error
 pause
--- make.bat
+++ make.bat
@@ -2,6 +2,11 @@
 mkdir derived\bin 2> NUL
 mkdir derived\lst 2> NUL
 cd src
+IF %1.==pasmo. GOTO PASM
 FOR %%i IN (sub;main) DO call ..\build %%i
+GOTO END
+:PASM
+FOR %%i IN (sub;main) DO call ..\build %%i pasmo
+:END
 del *.tmp >> NUL
 cd ..
