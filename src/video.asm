a9b078d9354ca3b357923b6f7f784a0e3eb1a5ef
Albert Beevendorp <bifimsx@users.sourceforge.net>
2004-12-18 19:47:06 +0000

    fixed a bug in CHGCLR.

--- src/video.asm
+++ src/video.asm
@@ -1,9 +1,10 @@
-; $Id: video.asm,v 1.6 2004/12/18 05:03:58 mthuurne Exp $
+; $Id: video.asm,v 1.7 2004/12/18 18:50:31 mthuurne Exp $
 ; C-BIOS video routines
 ;
 ; Copyright (c) 2002-2003 BouKiCHi.  All rights reserved.
 ; Copyright (c) 2003 Reikan.  All rights reserved.
 ; Copyright (c) 2004 Maarten ter Huurne.  All rights reserved.
+; Copyright (c) 2004 Albert Beevendorp.  All rights reserved.
 ;
 ; Redistribution and use in source and binary forms, with or without
 ; modification, are permitted provided that the following conditions
@@ -237,6 +238,7 @@ chgclr:
                 rlca
                 rlca
                 rlca
+		and	$F0
                 ld      l,a
                 ld      a,(BDRCLR)
                 or      l
