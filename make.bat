f1e3fc28c1169bb7865e6c02b4a19382cf17a4d3
Albert Beevendorp <bifimsx@users.sourceforge.net>
2005-01-02 10:42:40 +0000

    now compiles the version dependent mainroms

--- make.bat
+++ make.bat
@@ -3,10 +3,10 @@ mkdir derived\bin 2> NUL
 mkdir derived\lst 2> NUL
 cd src
 IF %1.==pasmo. GOTO PASM
-FOR %%i IN (sub;main;music;disk) DO call ..\build %%i
+FOR %%i IN (main_msx1;main_msx2;main_msx2+;sub;music;disk) DO call ..\build %%i
 GOTO END
 :PASM
-FOR %%i IN (sub;main;music;disk) DO call ..\build %%i pasmo
+FOR %%i IN (main_msx1;main_msx2;main_msx2+;sub;music;disk) DO call ..\build %%i pasmo
 :END
 del *.tmp >> NUL
 cd ..
