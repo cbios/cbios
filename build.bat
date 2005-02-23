2a7ec2dee1af34464bfe3118506ef024a19276e6
BouKiCHi <alf_bincyou@hotmail.com>
2005-02-23 21:57:54 +0000

    rewritten make.bat and build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,15 @@
+@echo off
+IF %1.==. GOTO NOOPT
 IF %2.==pasmo. GOTO PASM
 tniasm %1.asm ..\derived\bin\cbios_%1.rom
 if errorlevel 1 goto error
 move tniasm.sym ..\derived\lst\cbios_%1.sym
 goto no_error
+:NOOPT
+echo Error! build.bat should calls from make.bat,please run make.bat
+GOTO no_error
 :PASM
-pasmo %1.asm ..\derived\bin\cbios_%1.rom
+pasmo -d %1.asm ..\derived\bin\cbios_%1.rom > ..\derived\lst\cbios_%1.sym
 if errorlevel 1 goto error
 goto no_error
 :error
--- make.bat
+++ make.bat
@@ -1,8 +1,14 @@
 @echo off
 mkdir derived\bin 2> NUL
 mkdir derived\lst 2> NUL
+IF %1.==. GOTO NOOPT
 cd src
 IF %1.==pasmo. GOTO PASM
+IF %1.==tniasm. GOTO TASM
+:NOOPT
+echo usage make.bat [pasmo or tniasm]
+GOTO QUIT
+:TASM
 FOR %%i IN (main_msx1;main_msx2;main_msx2+;sub;music;disk) DO call ..\build %%i
 GOTO END
 :PASM
@@ -10,3 +16,4 @@ FOR %%i IN (main_msx1;main_msx2;main_msx2+;sub;music;disk) DO call ..\build %%i 
 :END
 del *.tmp >> NUL
 cd ..
+:QUIT
