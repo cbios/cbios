6f0e4e97dbeaf746b9d11fc122d9148dfe471363
Maarten ter Huurne <maarten@treewalker.org>
2005-10-27 23:21:49 +0000

    Added C-BASIC.

--- make.bat
+++ make.bat
@@ -10,12 +10,12 @@ echo usage make.bat [pasmo or tniasm]
 GOTO QUIT
 :TASM
 FOR %%i IN (main_msx1;main_msx2;main_msx2+) DO call ..\build %%i
-FOR %%i IN (sub;music;disk;logo_msx1;logo_msx2;logo_msx2+) DO call ..\build %%i
+FOR %%i IN (basic;sub;music;disk;logo_msx1;logo_msx2;logo_msx2+) DO call ..\build %%i
 FOR %%i IN (main_msx1_jp;main_msx2_jp;main_msx2+_jp) DO call ..\build %%i
 GOTO DEL_TEMP
 :PASM
 FOR %%i IN (main_msx1;main_msx2;main_msx2+) DO call ..\build %%i pasmo
-FOR %%i IN (sub;music;disk;logo_msx1;logo_msx2;logo_msx2+) DO call ..\build %%i pasmo
+FOR %%i IN (basic;sub;music;disk;logo_msx1;logo_msx2;logo_msx2+) DO call ..\build %%i pasmo
 FOR %%i IN (main_msx1_jp;main_msx2_jp;main_msx2+_jp) DO call ..\build %%i pasmo
 GOTO END
 :DEL_TEMP
