cbios/cbios

commit 4cfc62e66987b3148ff802e400157b9c76d6a29e

Prevent too fast VRAM access on MSX1
Thanks to Grauw for the review and tips!

Before this fix, VDP VRAM access was too fast, which was at least
visible in the C-BIOS MSX1 logo, but probably also on many games using
C-BIOS MSX1. This wasn't an issue before openMSX implemented accurate
VDP VRAM access timing, but now it clearly showed...

src/video.asm
@@ -207,8 +207,14 @@ filvrm_cont:
                 ;       comes just after our RET, which is certain if the
                 ;       memory block written is large enough.
 filvrm_lp:
+        IF VDP = TMS99X8
+                ; wait (at least) 29 t-states between VRAM accesses
                 out     (VDP_DATA),a
+                dec b
+                jr      nz,filvrm_lp
+        ELSE
                 djnz    filvrm_lp
+        ENDIF
                 dec     c
                 jr      nz,filvrm_lp
                 ret
@@ -244,7 +250,13 @@ ldirmv_cont:
                 inc     a
                 ld      c,VDP_DATA
 ldirmv_lp:
+        IF VDP = TMS99X8
+                ; wait (at least) 29 t-states between VRAM accesses
+                ini
+                jp nz, ldirmv_lp
+        ELSE
                 inir
+        ENDIF
                 dec     a
                 jr      nz,ldirmv_lp
                 pop     hl
@@ -280,7 +292,13 @@ ldirvm_cont:
                 inc     a
                 ld      c,VDP_DATA
 ldirvm_lp:
+        IF VDP = TMS99X8
+                ; wait (at least) 29 t-states between VRAM accesses
+                outi
+                jp nz, ldirvm_lp
+        ELSE
                 otir
+        ENDIF
                 dec     a
                 jr      nz,ldirvm_lp
                 ; Note: Without this, Quinpl shows glitches.
@@ -515,8 +533,17 @@ clrspr_attr_lp:
                 ld      a,e
                 out     (VDP_DATA),a    ; Y coordinate
                 ld      a,0
+        IF VDP = TMS99X8
+                nop                     ; wait (at least) 29 t-states between VRAM accesses
+                nop                     ; only 2 nops, as ld a,0 is slow
+        ENDIF
                 out     (VDP_DATA),a    ; X coordinate
                 ld      a,c
+        IF VDP = TMS99X8
+                nop                     ; wait (at least) 29 t-states between VRAM accesses
+                nop
+                nop
+        ENDIF
                 out     (VDP_DATA),a    ; pattern number
                 inc     c
                 call    gspsiz

commit d582d3a568efca8d27c3409520957b17472874c7

Fixed regression from 4cfc62e6: IF at wrong place
This caused the out to be compiled in only for MSX1 VDP's... oops.

src/video.asm
@@ -211,9 +211,9 @@ filvrm_cont:
                 ;       comes just after our RET, which is certain if the
                 ;       memory block written is large enough.
 filvrm_lp:
+                out     (VDP_DATA),a
         IF VDP = TMS99X8
                 ; wait (at least) 29 t-states between VRAM accesses
-                out     (VDP_DATA),a
                 dec b
                 jr      nz,filvrm_lp
         ELSE